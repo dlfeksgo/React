40860327a1b1d4fb674ea7cedf45d5b1566a6b34
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2022-03-31 19:43:13 +0900

    react-refresh 오류 해결😡

---
+++ NumberBaseball/numberBaseballClass.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NumberBaseball extends Component {
+	render() {
+		return (
+			<>
+				<p>제발 되자아</p>
+			</>
+		);
+	}
+}
+
+export default NumberBaseball;
