79465199fbace2fbfd08464f47592aa9efb7d704
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2022-04-06 23:28:56 +0900

    반응속도 Class Self

--- ResponseCheck/responseCheckClass.jsx
+++ ResponseCheck/responseCheckClass.jsx
@@ -1,5 +1,66 @@
 import React, { Component } from 'react';
 
-class ResponseCheck extends Component {}
+class ResponseCheck extends Component {
+	state = {
+		state: 'waiting',
+		message: '준비가 되면 클릭해주세요😀',
+		result: [],
+	};
+
+	timeId;
+	startTime;
+	endTime;
+
+	onClickScreen = () => {
+		const { state } = this.state;
+		switch (state) {
+			case 'waiting':
+				this.setState({
+					state: 'ready',
+					message: '초록 화면이 되면 클릭해주세요',
+				});
+				this.startTime = new Date();
+				this.timeId = setTimeout(() => {
+					this.setState({
+						state: 'now',
+						message: '지금이야!',
+					});
+				}, Math.floor(Math.random() * 1000) + 1000);
+				break;
+			case 'ready':
+				this.setState({
+					state: 'waiting',
+					message: '성급했어~',
+				});
+				clearTimeout(this.timeId);
+				break;
+			case 'now':
+				this.endTime = new Date();
+				this.setState((prevState) => {
+					return {
+						state: 'waiting',
+						message: '준비가 되면 클릭해주세요😀',
+						result: [...prevState.result, this.endTime - this.startTime],
+					};
+				});
+		}
+	};
+
+	render() {
+		const { state, message, result } = this.state;
+		return (
+			<>
+				<div id="screen" className={state} onClick={this.onClickScreen}>
+					{message}
+				</div>
+				<div>
+					{result.length === 0 ? null : (
+						<div>평균 {result.reduce((a, b) => a + b) / result.length} ms</div>
+					)}
+				</div>
+			</>
+		);
+	}
+}
 
 export default ResponseCheck;
