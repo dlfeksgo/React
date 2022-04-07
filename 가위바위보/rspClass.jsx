8ad2a1e666cfdd04f221e96fa5ccea083ba89328
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2022-04-07 17:43:47 +0900

    가위바위보 class self 작성

---
+++ 가위바위보/rspClass.jsx
@@ -0,0 +1,154 @@
+import React, { Component } from 'react';
+
+const IMG_URL = './public/assets/rsp.png';
+const rspCoords = {
+	scissors: '0',
+	rock: '-220px',
+	paper: '-440px',
+};
+
+class RspGame extends Component {
+	state = {
+		result: '',
+		imgCoord: 0,
+		score: 0,
+	};
+
+	interval;
+
+	componentDidMount() {
+		this.interval = setInterval(this.computerChange, 50);
+	}
+
+	componentWillMount() {
+		clearInterval(this.interval);
+	}
+
+	computerChange = () => {
+		const { imgCoord } = this.state;
+		if (imgCoord === rspCoords.scissors) {
+			this.setState({
+				imgCoord: rspCoords.rock,
+			});
+		} else if (imgCoord === rspCoords.rock) {
+			this.setState({
+				imgCoord: rspCoords.paper,
+			});
+		} else {
+			this.setState({
+				imgCoord: rspCoords.scissors,
+			});
+		}
+	};
+
+	onClickBtn = (e) => {
+		clearInterval(this.interval);
+		const myChoice = e.target.id;
+		const { imgCoord } = this.state;
+		if (myChoice === 'scissors') {
+			switch (imgCoord) {
+				case rspCoords.scissors:
+					this.setState({
+						result: '비겼습니다.',
+					});
+					break;
+				case rspCoords.rock:
+					this.setState((prevState) => {
+						return {
+							result: '졌습니다.',
+							score: prevState.score - 1,
+						};
+					});
+					break;
+				case rspCoords.paper:
+					this.setState((prevState) => {
+						return {
+							result: '이겼습니다.',
+							score: prevState.score + 1,
+						};
+					});
+					break;
+			}
+		} else if (myChoice === 'rock') {
+			switch (imgCoord) {
+				case rspCoords.scissors:
+					this.setState((prevState) => {
+						return {
+							result: '이겼습니다.',
+							score: prevState.score + 1,
+						};
+					});
+					break;
+				case rspCoords.rock:
+					this.setState({
+						result: '비겼습니다.',
+					});
+					break;
+				case rspCoords.paper:
+					this.setState((prevState) => {
+						return {
+							result: '졌습니다.',
+							score: prevState.score - 1,
+						};
+					});
+					break;
+			}
+		} else if (myChoice === 'paper') {
+			switch (imgCoord) {
+				case rspCoords.scissors:
+					this.setState((prevState) => {
+						return {
+							result: '졌습니다.',
+							score: prevState.score - 1,
+						};
+					});
+					break;
+				case rspCoords.rock:
+					this.setState((prevState) => {
+						return {
+							result: '이겼습니다.',
+							score: prevState.score + 1,
+						};
+					});
+					break;
+				case rspCoords.paper:
+					this.setState({
+						result: '비겼습니다.',
+					});
+					break;
+			}
+		}
+		setTimeout(() => {
+			this.interval = setInterval(this.computerChange, 50);
+		}, 2000);
+	};
+
+	render() {
+		const { result, score, imgCoord } = this.state;
+		return (
+			<>
+				<div
+					id='computer'
+					style={{
+						background: `url(./public/assets/rsp.png) ${imgCoord} 0/auto 200px `,
+					}}
+				></div>
+				<div>
+					<button id='scissors' className='btn' onClick={this.onClickBtn}>
+						가위
+					</button>
+					<button id='rock' className='btn' onClick={this.onClickBtn}>
+						바위
+					</button>
+					<button id='paper' className='btn' onClick={this.onClickBtn}>
+						보
+					</button>
+				</div>
+				<div>{result}</div>
+				<div id='score'>{score}</div>
+			</>
+		);
+	}
+}
+
+export default RspGame;
---
+++ 가위바위보/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = {
+	name: 'numberBaseball-setting',
+	mode: 'development',
+	devtool: 'eval',
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+	entry: {
+		app: ['./client'],
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-env', '@babel/preset-react'],
+					plugins: ['react-refresh/babel'],
+				},
+			},
+		],
+	},
+
+	plugins: [new RefreshWebpackPlugin()],
+
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'app.js',
+		publicPath: '/dist',
+	},
+	devServer: {
+		devMiddleware: { publicPath: '/dist' }, //webpack이 생성해준 파일들의 경로
+		static: { directory: path.resolve(__dirname) }, //실제로 존재하는 정적파일들의 정보
+		hot: true,
+	},
+};
