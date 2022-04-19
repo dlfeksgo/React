550272f9355ec758a653f843b2dc9e1cc2304aa4
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2022-04-19 19:05:31 +0900

    함수 분리 방식으로 Create 해결

--- Study/Test.jsx
+++ Study/Test.jsx
@@ -3,7 +3,7 @@ import React, { useCallback, useState } from 'react';
 const Header = ({ title, onChangeMode }) => {
 	return (
 		<h1>
-			<a href='/' onClick={onChangeMode}>
+			<a href="/" onClick={onChangeMode}>
 				{title}
 			</a>
 		</h1>
@@ -42,46 +42,105 @@ const Article = ({ title, body }) => {
 	);
 };
 
+const Create = ({ onCreate }) => {
+	return (
+		<>
+			<h1>Create</h1>
+			<form
+				onSubmit={(e) => {
+					e.preventDefault();
+					const title = e.target.title.value;
+					const body = e.target.body.value;
+					onCreate(title, body);
+				}}
+			>
+				<p>
+					<input name="title" type="text" placeholder="title" />
+				</p>
+				<p>
+					<textarea
+						name="body"
+						id=""
+						cols="20"
+						rows="2"
+						placeholder="body"
+					></textarea>
+				</p>
+				<button type="submit">Create</button>
+			</form>
+		</>
+	);
+};
+
 const Test = () => {
 	const [mode, setMode] = useState('WELCOME');
 	const [id, setId] = useState(null);
-
-	const topics = [
+	const [nextId, setNextId] = useState(4);
+	const [topics, setTopics] = useState([
 		{ id: 1, title: 'html', body: 'html is ...' },
 		{ id: 2, title: 'css', body: 'css is ...' },
 		{ id: 3, title: 'javascript', body: 'javascript is ...' },
-	];
+	]);
 
-	const onChangeModeHeader = (e) => {
-		e.preventDefault();
-		setMode('WELCOME');
-	};
+	const onChangeModeHeader = useCallback(
+		(e) => {
+			e.preventDefault();
+			setMode('WELCOME');
+		},
+		[mode]
+	);
 
-	const onChangeModeNav = (e) => {
-		e.preventDefault();
-		setMode('READ');
-		setId(e.target.id);
-	};
+	const onChangeModeNav = useCallback(
+		(e) => {
+			e.preventDefault();
+			setMode('READ');
+			setId(Number(e.target.id));
+		},
+		[mode, id]
+	);
+
+	const onChangeModeCreateList = useCallback(
+		(e) => {
+			e.preventDefault();
+			setMode('CREATE');
+		},
+		[mode]
+	);
+
+	const onCreateList = useCallback(
+		(_title, _body) => {
+			const newTopic = { id: nextId, title: _title, body: _body };
+			const newTopics = [...topics];
+			newTopics.push(newTopic);
+			setTopics(newTopics);
+			setMode('READ');
+			setId(nextId);
+			setNextId(nextId + 1);
+		},
+		[topics, mode, id, nextId]
+	);
 
 	let content = null;
 	if (mode === 'WELCOME') {
-		content = <Article title='Welcome' body='Welcome이닷!' />;
+		content = <Article title="Welcome" body="Welcome이닷!" />;
 	} else if (mode === 'READ') {
-		let title, body;
 		topics.map((v) => {
 			if (v.id === id) {
-				title = v.title;
-				body = v.body;
+				content = <Article title={v.title} body={v.body} />;
 			}
 		});
-		content = <Article title={title} body={body} />;
+	} else if (mode === 'CREATE') {
+		content = <Create onCreate={onCreateList} />;
 	}
 
 	return (
 		<>
-			<Header title='REACT' onChangeMode={onChangeModeHeader} />
+			<Header title="REACT" onChangeMode={onChangeModeHeader} />
 			<Nav topics={topics} onChangeMode={onChangeModeNav} />
 			{content}
+			<a href="/create" onClick={onChangeModeCreateList}>
+				create
+			</a>
 		</>
 	);
 };
